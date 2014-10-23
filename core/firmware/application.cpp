9c0cddedf1c6f54d9003f21eedaad28cbe6cf213
delianides <drew@delianides.com>
2014-10-23 09:43:12 -0400

    more adjustments, standardized naming of functions

--- core/firmware/application.cpp
+++ core/firmware/application.cpp
@@ -12,32 +12,30 @@
   necessary, but will require a fast device such as one of the following:
 
   [SN74HCT125N]
-  http://www.digikey.com/product-detail/en/SN74HCT125N/296-8386-5-ND/376860
+http://www.digikey.com/product-detail/en/SN74HCT125N/296-8386-5-ND/376860
 
-  [SN74HCT245N]
-  http://www.digikey.com/product-detail/en/SN74HCT245N/296-1612-5-ND/277258
+[SN74HCT245N]
+http://www.digikey.com/product-detail/en/SN74HCT245N/296-1612-5-ND/277258
 
-  [TXB0108PWR]
-  http://www.digikey.com/product-search/en?pv7=2&k=TXB0108PWR
+[TXB0108PWR]
+http://www.digikey.com/product-search/en?pv7=2&k=TXB0108PWR
 
-  If you have a Spark Shield Shield, the TXB0108PWR 3.3V to 5V level
-  shifter is built in.
+If you have a Spark Shield Shield, the TXB0108PWR 3.3V to 5V level
+shifter is built in.
 
-  Written by Phil Burgess / Paint Your Dragon for Adafruit Industries.
-  Modified to work with Spark Core by Technobly.
-  Contributions by PJRC and other members of the open source community.
+Written by Phil Burgess / Paint Your Dragon for Adafruit Industries.
+Modified to work with Spark Core by Technobly.
+Contributions by PJRC and other members of the open source community.
 
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing products
-  from Adafruit!
-  --------------------------------------------------------------------*/
+Adafruit invests time and resources providing this open source code,
+please support Adafruit and open-source hardware by purchasing products
+from Adafruit!
+--------------------------------------------------------------------*/
 
 /* ======================= includes ================================= */
 
 #include "application.h"
-//#include "spark_disable_wlan.h" (for faster local debugging only)
 #include "neomatrix.h"
-
 #include "flashee-eeprom.h"
 using namespace Flashee;
 
@@ -49,15 +47,27 @@ using namespace Flashee;
 SYSTEM_MODE(SEMI_AUTOMATIC);
 
 //Definition for extra functions
-int setSalvationCount(String count);
-void printSalvation(int salvations);
+//
+//RESTful function for receiving data
+int setSalvations(String count);
+
+//Print Salvations
+void showSalvations(int salvations, int oldSalvations);
+
+//Helper function to count the number of digits so the cursor moves.
 int countDigits(int digits);
+
+// Turn LED on for no wifi...
+void missingWifi();
+
+// Turn LED on for low battery...
+void lowBattery();
 
 //Define Matrix
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8,8,5,1, PIXEL_PIN,
- NEO_TILE_TOP   + NEO_TILE_LEFT   + NEO_TILE_ROWS   + NEO_TILE_PROGRESSIVE +
- NEO_MATRIX_TOP + NEO_MATRIX_LEFT + NEO_MATRIX_ROWS + NEO_MATRIX_PROGRESSIVE
-);
+    NEO_TILE_TOP   + NEO_TILE_LEFT   + NEO_TILE_ROWS   + NEO_TILE_PROGRESSIVE +
+    NEO_MATRIX_TOP + NEO_MATRIX_LEFT + NEO_MATRIX_ROWS + NEO_MATRIX_PROGRESSIVE
+    );
 
 void setup() {
   //Initalize the reader
@@ -77,7 +87,7 @@ void setup() {
   int salvations = reader.readInt();
 
   if(salvations){
-    printSalvation(salvations);
+    showSalvations(salvations, 0);
   }else{
     matrix.print(F("******"));
     matrix.show();
@@ -92,47 +102,61 @@ void loop() {
   if ( Spark.connected() == true){
     //Enable Wifi
     Spark.publish("ticker-connected");
-    Spark.function("celebrate", setSalvationCount);
+    Spark.function("celebrate", setSalvations);
+  }else{
+    missingWifi();
   }
+
+  //Read analog input for battery life.
 }
 
-int setSalvationCount(String count){
+int setSalvations(String count){
   //Convert count string to integer.
   int salvations = atoi(count.c_str());
 
   //Initalize the flash device for saving the new number
   FlashDevice* device = Devices::createWearLevelErase();
+
+  //Get the old number.
+  FlashReader reader(device);
+  int oldSalvations = reader.readInt();
+
+  //Write the new number.
   FlashWriter writer(device);
   writer.writeInt(salvations);
 
-  //Output Serial for debug
+  //Output Serial for debug.
   Serial.print("Salvations: ");
   Serial.println(salvations);
 
-  printSalvation(salvations);
+  //Fun Print function
+  showSalvations(salvations, oldSalvations);
 
   //Restful function returns count
   return salvations;
 }
 
-void printSalvation(int salvations){
+void showSalvations(int salvations, int oldSalvations){
   //Initalize Digits
   int digits = 1;
-  //Random number to increment by so it doesn't seem so neat.
-  int flux = rand() % 30 + 50;
-  //Loop through a counter until it reaches salvations
-  for(int c = 0; c <= salvations; c = c + flux){
-    Serial.print("Count: ");
-    Serial.println(c);
+  //Loop through a counter until it reaches salvations. This loop happens very fast.
+  for(int c = oldSalvations; c <= salvations; c = c + (rand() % 30 + 200)){
+    //Fill Screen with black
     matrix.fillScreen(0);
+    //Count digits
     digits = countDigits(c);
+    //Place cursor
     matrix.setCursor(41-(digits*6),0);
+    //set the data within the matrix
     matrix.print(F(c));
+    //Show
     matrix.show();
-    flux = rand() % 30 + 200;
+    //delay the next interation for 35ms
     delay(35);
   }
 
+  //The following happens at the very end but because the ticker is moving so fast you wouldn't see it happen.
+  //This is just to ensure the final number displayed is the correct total.
   matrix.fillScreen(0);
   digits = countDigits(salvations);
   matrix.setCursor(41-(digits*6),0);
@@ -152,11 +176,13 @@ int countDigits(int number){
   return digits;
 }
 
-// void missingWifi(){
-//   //light pixel for no wifi
-// }
-//
-// void lowBattery(){
-//   //light pixel for low battery
-// }
+void missingWifi(){
+  matrix.drawPixel(1,8,matrix.Color(255,255,35));
+  matrix.show();
+}
+
+void lowBattery(){
+  matrix.drawPixel(0,8,matrix.Color(255,70,0));
+  matrix.show();
+}
 
